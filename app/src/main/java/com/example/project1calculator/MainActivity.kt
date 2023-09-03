810ba0b3ee0c0cef5d9b821615b22709abe585ca
PeteSullivan <pewisull@iu.edu>
2023-09-03 13:45:51 -0400

    Built UI, began adding button functionality

--- app/src/main/java/com/example/project1calculator/MainActivity.kt
+++ app/src/main/java/com/example/project1calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.project1calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.TestLooperManager
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("hello world!")
+
+        // get reference to button
+        val button0 = findViewById(R.id.button0) as Button
+        val display = findViewById(R.id.display) as TextView
+        // set on-click listener
+        button0.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            display.setText("0")
+        }
+
+
     }
+
+
 }
