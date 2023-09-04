c3cd089f85b01c1a798f2dcd2a7f431e8a20d2de
PeteSullivan <pewisull@iu.edu>
2023-09-03 20:02:28 -0400

    added 1, +, C, and = functionality v1

--- app/src/main/java/com/example/project1calculator/MainActivity.kt
+++ app/src/main/java/com/example/project1calculator/MainActivity.kt
@@ -5,22 +5,104 @@ import android.os.Bundle
 import android.os.TestLooperManager
 import android.widget.Button
 import android.widget.TextView
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("hello world!")
 
-        // get reference to button
-        val button0 = findViewById(R.id.button0) as Button
-        val display = findViewById(R.id.display) as TextView
-        // set on-click listener
-        button0.setOnClickListener {
-            // your code to perform when the user clicks on the button
-            display.setText("0")
+
+        var displayText = ""
+        var isLastPressedSign = false
+        var lastSign = '='
+        var storage = 0.0
+
+
+        // get reference to buttons
+        val button0 = findViewById<Button>(R.id.button0)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val buttonc = findViewById<Button>(R.id.buttonc)
+        val buttonadd = findViewById<Button>(R.id.buttonadd)
+        val buttondivide = findViewById<Button>(R.id.buttondivide)
+        val buttonmultiply = findViewById<Button>(R.id.buttonmultiply)
+        val buttonsubtract = findViewById<Button>(R.id.buttonsubtract)
+        val buttondecimal = findViewById<Button>(R.id.buttondecimal)
+        val buttonpercent = findViewById<Button>(R.id.buttonpercent)
+        val buttonrun = findViewById<Button>(R.id.buttonrun)
+        val buttonsign = findViewById<Button>(R.id.buttonsign)
+
+        val display = findViewById<TextView>(R.id.display)
+
+        //button functions
+        button1.setOnClickListener {
+            if (isLastPressedSign) {
+                isLastPressedSign = false
+                displayText = ""
+            }
+            if (displayText.length < 8) {
+                displayText += "1"
+                display.setText(displayText)
+            }
         }
+
+        buttonc.setOnClickListener {
+            displayText = ""
+            display.setText(displayText)
+            lastSign = '='
+            isLastPressedSign = false
+        }
+        buttonadd.setOnClickListener {
+            if (isLastPressedSign && lastSign == '=') lastSign = '+'
+            else if (isLastPressedSign) {
+                var temp = displayText.toDouble()
+
+                if (lastSign == '+') storage = storage + temp
+                else if (lastSign == '-') storage = storage - temp
+                else if (lastSign == '/') storage = storage / temp
+                else if (lastSign == '*') storage = storage * temp
+
+
+                displayText = storage.toString().substring(0, minOf(storage.toString().length, 8))
+                display.setText(displayText)
+                storage = temp
+            }
+            else {
+                storage = displayText.toDouble()
+                isLastPressedSign = true
+                lastSign = '+'
+            }
+        }
+        buttonrun.setOnClickListener {
+            if (!isLastPressedSign && lastSign != '=') {
+                var temp = displayText.toDouble()
+
+                if (lastSign == '+') storage = storage + temp
+                else if (lastSign == '-') storage = storage - temp
+                else if (lastSign == '/') storage = storage / temp
+                else if (lastSign == '*') storage = storage * temp
+
+
+                displayText = storage.toString().substring(0, minOf(storage.toString().length, 8))
+                display.setText(displayText)
+                storage = temp
+                isLastPressedSign = true
+
+            }
+
+        }
+
 
 
     }
